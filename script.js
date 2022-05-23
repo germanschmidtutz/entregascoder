10ed6c6c9056424f3beebb82ab1a30dd06153920
sergio biscossa <germanchumi@gmail.com>
2022-05-23 18:19:54 -0300

    Incorporando Arrays

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-// variables
+
+// array de personas vacío
+const personas = [];
+// se consulta la cant de personas
+let cantPersonas = prompt("¿Cuántas personas desea cargar?");
 
 
-let precioRemera = 3000;
+if (isNaN(cantPersonas)){
+    alert("No escribiste un número")
+}else{
+   
+    // for para pedir los nombres de la cantidad de las personas indicadas
+    for (let i = 1; i <= cantPersonas; i++){
+        personas.push(prompt("Decime el nombre de la persona número " + i));
+    }
+  
+    // se da la lista
+       alert ("Esta es la lista de personas: \n" + personas.join(", "));
+    }
 
-let tieneCupon = prompt("Tenés un cupón de descuento para esta remera? Si/No");
-tieneCupon = tieneCupon.toUpperCase();
 
-function compra(precioremera, tieneCupon) {
 
-  if (tieneCupon == "SI") {
-    let cupon = prompt("Ingresá tu cupón");
-    cupon = cupon.toUpperCase();
 
-    if (cupon == "DESCUENTO") {
-      let preciofinal = precioremera - 200;
-      alert("Descuento aplicado. El precio final es: $" + preciofinal);
 
-    }else{
-        alert("Cupón inválido")
 
-}
-  } else if (tieneCupon == "NO"){
-    alert("El precio final es $" + precioRemera);
-
-  } else{
-        alert("Solamente podés responder Si o no a esta pregunta")}
-        
-}
-
-compra(precioRemera, tieneCupon);
